1f97b231bd6a3cadea28a9c26a45ac667f8e6448
SL@container <hustsonglun@gmail.com>
2025-12-06 10:06:29 +0000

    save

--- JaxProof/Api.lean
+++ JaxProof/Api.lean
@@ -9,10 +9,20 @@ def curryType (α : Type) : Nat → Type
 
 class Impl (α : ℕ → Type) where
   protected mul {n : ℕ} : α n → α n → α n
+  protected cast {n m : ℕ} : α n → n = m → α m
   lift {n : ℕ} (m : ℕ) : α n → α (n + m)
   fori_loop {n : ℕ} : α n → α n → (α (n + 2) → α (n + 2) → α (n + 2)) → α n
 
-instance (α : ℕ → Type) [Impl α] (n : ℕ) : Mul (α n) where mul := Impl.mul
+instance (α : ℕ → Type) [Impl α] (n m : ℕ) : HMul (α n) (α m) (α (max n m)) where
+  hMul x y := 
+    if h : n ≤ m then
+      let x' := Impl.lift (m - n) x
+      let y' := Impl.cast y (Nat.add_sub_of_le h).symm
+      Impl.cast (Impl.mul x' y') add_tsub_eq_max
+    else
+      let x' := Impl.cast x (Nat.add_sub_of_le (lt_of_not_ge h).le).symm
+      let y' := Impl.lift (n - m) y
+      Impl.cast (Impl.mul x' y') (max_comm n m ▸ add_tsub_eq_max)
 
 structure Tracer (n : ℕ) where
   expr : Expr
@@ -20,9 +30,10 @@ structure Tracer (n : ℕ) where
 instance ImplTracer : Impl Tracer where
   mul x y := ⟨x.expr.mul y.expr⟩
   lift m x := ⟨Nat.repeat Expr.lift m x.expr⟩
+  cast x _ := ⟨x.expr⟩
   fori_loop n x f := ⟨Expr.fori_loop n.expr x.expr (.func 2 (f ⟨Expr.arg 0⟩ ⟨Expr.arg 1⟩).expr)⟩
 
-def trace (n : ℕ) (f : {α : ℕ → Type} → [Impl α] → curryType (α n) n) : String :=
+def trace {n : ℕ} (f : {α : ℕ → Type} → [Impl α] → curryType (α n) n) : String :=
   let α := Tracer n
   let rec feed {m : ℕ} (f : curryType α m) : α :=
     match m with
@@ -34,6 +45,7 @@ def trace (n : ℕ) (f : {α : ℕ → Type} → [Impl α] → curryType (α n) 
 instance ImplArray : Impl (fun _ ↦ Array) where
   mul := Array.mul
   lift _ := id
+  cast x _ := x
   fori_loop n x f := match n with
     | .int [n] => Nat.rec x (fun i a ↦ f (.int [i]) a) n.natAbs
     | _ => .error
@@ -44,9 +56,26 @@ open Jax.Impl
 
 variable {α : ℕ → Type} [Jax.Impl α]
 
-def f (n x : α 2) : α 2 :=
-  let g (_ a : α 4) : α 4 := a * lift 2 x
-  fori_loop n x g
+/-
 
-#eval IO.println (Jax.trace 2 f)
+I want to define a DSL supporting following syntax
+
+```
+jax_def f(n, x):
+  jax_def g(i, a):
+    return a * x
+  return fori_loop n x g
+```
+
+which would be expanded to the following code
+
+-/
+
+def f : Jax.curryType (α 2) 2 := fun n x ↦
+  let _n_arg : ℕ := 2 -- or maybe we don't need this
+  let g : Jax.curryType (α (_n_arg + 2)) 2 := fun i a ↦
+    @id (α (_n_arg + 2)) (a * x)
+  @id (α 2) (fori_loop n x g)
+
+#eval IO.print (Jax.trace f)
 
