10ac0ef1053a4d119b1363b0a705429439f7c45c
SL@container <hustsonglun@gmail.com>
2026-01-29 05:26:44 +0000

    save

--- Attention.lean
+++ Attention.lean
@@ -10,31 +10,44 @@ jax_def (n₁ : ℕ) (n₂ : ℕ) softmax(x):
 
 noncomputable def softmax_def {n₁ n₂ : ℕ} (x : Matrix (Fin n₁) (Fin n₂) ℝ) :
     Matrix (Fin n₁) (Fin n₂) ℝ := 
-  Matrix.of fun i j ↦ Real.exp (x i j) / ∑ k, Real.exp (x i k)
+  Matrix.of fun i j ↦ Real.exp (x i j) / ∑ k, Real.exp (x k j)
 
 #print Finset.Nonempty
 
 #eval IO.println (Jax.trace (softmax 11 10)).code
 
+set_option maxHeartbeats 0 in
 theorem softmax_eq_def (n₁ n₂ : ℕ) (x : Matrix (Fin n₁) (Fin n₂) ℝ) (hn : n₁ ≠ 0 ∧ n₂ ≠ 0) :
     Jax.native (softmax n₁ n₂) (Jax.Array.ofMatrix x) = Jax.Array.ofMatrix (softmax_def x) := by
-  simp? [softmax, Jax.Array.ofMatrix, Jax.Array.ofTensor]
+  simp [softmax, Jax.Array.ofMatrix, Jax.Array.ofTensor]
   conv_lhs =>
     fun
-    simp [HDiv.hDiv]
+    simp only [HDiv.hDiv]
   simp [Jax.Array.div, Jax.Array.rep, Jax.Array.einsum, Jax.List.map?.go, Jax.Array.toTensor,
     Jax.Array.ofTensor, mul_comm n₁ n₂]
+
+  /-
   split_ifs with h
   · contrapose! h
     intro i
     simp [Jax.Tensor.einsum, Jax.Tensor.einprod, Jax.Tensor.unflatten, Jax.Tensor.flatten]
     intro hc
-    simp [Jax.Tensor] at hc
-    sorry
+    simp [Jax.Tensor, Fin.divNat] at hc
+    change (∑ i, fun i' ↦ Real.exp (x i i')) i.divNat = 0 at hc
+    revert hc
+    rw [Finset.sum_apply]
+    suffices hfinal : 0 < (∑ i', Real.exp (x i' i.divNat)) by
+      exact hfinal.ne.symm
+    apply Finset.sum_pos
+    · intro _ _
+      exact Real.exp_pos _
+    · have : NeZero n₁ := ⟨hn.1⟩
+      let i₀ : Fin n₁ := 0
+      use i₀
+      simp
   apply congrArg
   apply congrArg
   ext i
-  --simp [softmax_def, Jax.Tensor.einsum, Jax.Tensor.einprod, Jax.Tensor.unflatten, Jax.Tensor.flatten]
   conv_lhs =>
     arg 2
     simp [Jax.Tensor.einsum, Jax.Tensor.einprod]
@@ -45,8 +58,11 @@ theorem softmax_eq_def (n₁ n₂ : ℕ) (x : Matrix (Fin n₁) (Fin n₂) ℝ) 
   conv_rhs =>
     simp [softmax_def, Jax.Tensor.flatten]
   apply congrArg
-  conv_lhs =>
-    change (∑ i, fun i' ↦ Real.exp (x i i')) i.divNat
+  simp [Jax.Tensor.flatten]
+  sorry
+
+-/
+  congr 1
 
 
 
