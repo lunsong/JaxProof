8f90ecafde726148a489bc8857a745bc457802aa
songlun <songlun@SHAI14001320.pjlab.org>
2026-03-10 18:52:29 +0800

    abort new dot_general

--- JaxProof/Impl.lean
+++ JaxProof/Impl.lean
@@ -13,6 +13,9 @@ def FloatAsReal.zero {σ : TensorType} : FloatAsReal σ :=
 
 instance (σ : TensorType) : Zero (FloatAsReal σ) := ⟨FloatAsReal.zero⟩
 
+#print Tensor.einprod
+#check Tensor.transpose
+
 noncomputable instance : TensorImpl FloatAsReal where
   ofNat i := Int.ofNat i
   impl {args} {out} op := match op with
@@ -28,8 +31,23 @@ noncomputable instance : TensorImpl FloatAsReal where
   | .mul => fun ⟨x, y, _⟩ => match out with
     | ⟨.float, _⟩
     | ⟨.int, _⟩ => Tensor.map₂ (· * ·) x y
-  | .dot_general batch contract lhs_indep rhs_indep lhs rhs lhs_inj lhs_surj rhs_inj rhs_surj =>
-    sorry
+  | .dot_general (α := α) batch contract lhs_indep rhs_indep lhs rhs lhs_inj lhs_surj rhs_inj rhs_surj =>
+    fun ⟨x, y, _⟩ =>
+    match α with
+    | .int => sorry
+    | .float =>
+      --let lhs_shape := _dot_general.get_shape batch contract lhs_indep lhs
+      --let rhs_shape := _dot_general.get_shape batch contract rhs_indep rhs
+      let σ : Fin lhs.length → Fin (contract ++ batch ++ lhs_indep).length := fun i ↦
+        match lhs[i] with
+        | .inl j => Fin.mk j <| by simp; omega
+        | .inr (.inl j) => Fin.mk (batch.length + j) <| by simp; omega
+        | .inr (.inr j) => Fin.mk (batch.length + contract.length + j) <| by simp; omega
+      have σ_inj : Function.Injective σ := by sorry
+      have σ_surj : Function.Surjective σ := by sorry
+      let σ : Equiv
+      --let x' : Tensor ℝ (contract ++ batch ++ lhs_indep) := x.transpose
+      sorry
   | _ => 0
 
 
