6200aa0228091f460fdcafa7be3a7c53456a4e8c
songlun <songlun@SHAI14001320.pjlab.org>
2026-03-23 14:43:48 +0800

    scatter works

--- Test/scatter.lean
+++ Test/scatter.lean
@@ -6,7 +6,7 @@ def inv {n : ℕ} :=
   returns
     int [n]
   begin
-    return .bind .scatter *[0, x, Jax.iota]
+    return .bind .scatter *[0, Jax.iota, x]
 
 #eval IO.println (inv (n := 10)).code
 
@@ -17,57 +17,46 @@ theorem inv_def (n : ℕ) (σ : Equiv.Perm (Fin n)) :
   match n with
   | 0 => by
     intro x y
-    simp [Jax.ExprGroup.eval, inv]
+    simp only [inv, List.length_cons, List.length_nil, Nat.reduceAdd, List.replicate_one,
+      List.replicate_zero, Fin.isValue, Jax.ExprGroup.eval, Jax.DList.cons.injEq, and_true]
     ext i
     nomatch i
   | n + 1 => by
     intro x y
-    simp [inv, Jax.ExprGroup.eval, Jax.Expr.eval, Jax.TensorImpl.impl, Jax.Expr.eval.recursive_eval]
-    let ι : Jax.FloatAsReal ⟨.int, [n + 1]⟩ := Jax.Expr.eval Jax.FloatAsReal (Jax.DList.cons x Jax.DList.nil) Jax.iota
-    change Jax.FloatAsReal.scatter (s := [n + 1]) 0 x *[ι] = y
-    --have : σ ∈ (⊤ : Submonoid (Equiv.Perm (Fin (n + 1)))) := by simp
-    --rw [← Equiv.Perm.mclosure_swap_castSucc_succ] at this
-    --refine Submonoid.closure_induction  ?_ ?_ ?_ this
-    simp [Jax.FloatAsReal.scatter]
+    simp only [inv, List.length_cons, List.length_nil, Nat.reduceAdd, List.replicate_one,
+      List.replicate_zero, Fin.isValue, Jax.ExprGroup.eval, Jax.Expr.eval, Jax.TensorImpl.impl,
+      Jax.Expr.eval.recursive_eval, Pi.zero_apply, Jax.FloatAsReal.scatter, Fin.getElem_fin,
+      Fin.val_eq_zero, List.getElem_cons_zero, ne_eq, Nat.add_eq_zero_iff, one_ne_zero, and_false,
+      not_false_eq_true, implies_true, ↓reduceDIte, Function.comp_apply, Jax.DList.cons.injEq,
+      and_true]
+    rw [← Jax.Tensor.of_get (x := y)]
+    congr
+    ext idx
+    let indices :=
+      (Jax.ValidIdx.intCast (s := [n + 1]) (by simp)) ∘
+        Function.swap (Jax.DList.unfold_replicate (n := 1) *[x])
+    let j := Fin.find? fun i => idx = indices i
     conv_lhs =>
-      arg 1; intro i r
-      arg 1
-      simp [Jax.FloatAsReal.scatter.get_indices]
-      rw [show r = 0 by omega]
-      simp
-
-    let fn {n : ℕ} (x ι : Fin (n + 1) → ℤ) : Fin (n + 1) → ℤ :=
-      let ι' : Fin (n + 1) → Jax.ValidIdx [n + 1] := fun i r =>
-        have : NeZero [n + 1][r.val] := .mk <| by simp
-        Fin.intCast (ι i)
-      Jax.FloatAsReal.scatter.update ι' 0 x
-    change fn x ι = y
-
-    let z : Fin (n + 1) → ℤ := fun i => σ.symm i
-
-    suffices h1 : ∀ σ : Equiv.Perm (Fin (n + 1)), ∀ (x ι : Fin (n + 1) → ℤ),
-      fn (x ∘ σ) (ι ∘ σ) = fn x ι by
-      specialize h1 σ.symm x ι
-      conv_lhs at h1 =>
-        conv =>
-          arg 1
-          change fun i => σ (σ.symm i)
-          simp
-        arg 2
-        change fun i => σ.symm i
-      rw [← h1]
-      suffices h2 : ∀ n, ∀ x : Fin (n + 1) → ℤ, fn (fun i => i) x = x by
-        rw [h2]
-        rfl
-      intro n
-      induction n with
-      | zero =>
-        intro x
-        simp [fn]
-        ext i
-    sorry
-    --have h1 (σ : Equiv.Perm (Fin (n + 1))) (x ι : Fin (n + 1) → ℤ) :
-    --  fn (x ∘ σ) (ι ∘ σ) = (fn x ι) ∘ σ := sorry
-
-
-
+      arg 2
+      change j
+    have : j = some (σ.symm (idx 0)) := by
+      simp only [Fin.isValue, Fin.findSome?_eq_some_iff, Option.guard_eq_some_iff,
+        decide_eq_true_eq, Option.guard_eq_none_iff, decide_eq_false_iff_not, ↓existsAndEq,
+        true_and, j]
+      have : NeZero ([n + 1].get 0) := .mk <| by simp
+      constructor
+      · apply funext
+        intro r
+        rw [show r = 0 by simp; omega]
+        change idx 0 = Fin.intCast (σ (σ.symm (idx 0)))
+        simp [Fin.intCast]
+      · intro k hk hc
+        rw [funext_iff] at hc
+        specialize hc 0
+        change idx 0 = Fin.intCast (σ k) at hc
+        simp [Fin.intCast] at hc
+        apply hk.ne
+        rw [Equiv.eq_symm_apply]
+        exact hc.symm
+    rw [this]
+    rfl
