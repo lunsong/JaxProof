849f6b25c3bcf52fb0bc0fee7e0d0fe8da5b3fdc
songlun <songlun@SHAI14001320.pjlab.org>
2026-04-16 19:22:42 +0800

    save

--- JaxProof/Expr.lean
+++ JaxProof/Expr.lean
@@ -20,51 +20,14 @@ structure TensorType where
   dtype : DType
   shape : List ℕ
 
+def TensorType.int_scalar : TensorType := ⟨.int, []⟩
+
 instance : ToString DType where
   toString x := match x with | .float => "float" | .int => "int"
 
-/-
-inductive DList {α : Type} (m : α → Type) : List α → Type where
-  | nil : DList m []
-  | cons {x : α} {xs : List α} : m x → DList m xs → DList m (x :: xs)
-
-syntax "*[" term,* "]" : term
-open Lean in macro_rules
-  | `(term| *[ $[$items],* ]) =>
-    let rec parse : List (TSyntax `term) → MacroM (TSyntax `term)
-    | [] => `(DList.nil)
-    | x :: xs => do
-      let xs ← parse xs
-      `(DList.cons $x $xs) 
-    let ⟨items⟩ := items
-    parse items
-
-def DList.mapM {α β : Type} {γ : α → Type} {x : List α} {m : Type → Type} [Monad m]
-  (f : {x : α} → γ x → m β) : DList γ x → m (List β)
-  | nil => pure []
-  | cons a as => do
-    let a ← f a
-    let as ← as.mapM f
-    return a :: as
-
-def DList.get {α : Type} {γ : α → Type} {a : List α}
-    (i : Fin a.length) (x : DList γ a) : γ a[i] :=
-  match a with
-  | a :: as =>
-    match x with
-    | cons x xs =>
-      match i with
-      | .mk 0 _ => x
-      | .mk (n + 1) h => xs.get <| .mk n <| by simpa using h
-
-def DList.of {α : Type} {γ : α → Type} {a : List α} (x : ∀ i : Fin a.length, γ a[i]) : DList γ a :=
-  match a with
-  | [] => *[]
-  | _ :: _ => .cons (x 0) <| DList.of fun i => x i.succ
--/
-
 inductive Expr (opType : List TensorType → TensorType → Type) :
     List TensorType → List TensorType → Type where
+  | nil {args : List TensorType} : Expr opType args []
   | arg {args : List TensorType} (i : Fin args.length) : Expr opType args [args[i]]
   | bind {args : List TensorType} {ins : List TensorType} {out : TensorType} :
     opType ins out → Expr opType args ins → Expr opType args [out]
@@ -74,14 +37,20 @@ inductive Expr (opType : List TensorType → TensorType → Type) :
     Expr opType args outs → Expr opType args outs' → Expr opType args (outs ++ outs')
   | select {args outs : List TensorType} (i : List (Fin outs.length)) :
     Expr opType args outs → Expr opType args (i.map outs.get)
+  | fori_loop {args carry : List TensorType} :
+    ℕ → Expr opType (TensorType.int_scalar :: carry) carry
+      → Expr opType args carry
+      → Expr opType args carry
 
 variable {opType : List TensorType → TensorType → Type} [∀ args, ∀ out, ToString (opType args out)]
 
 abbrev Cached (α : Type) : Type := List (USize × α)
 
+abbrev Expr.CodeM (α : Type) : Type :=
+  StateM (ℕ × Cached (List ℕ × String) × Cached String) α
+
 unsafe def Expr.addLine {args outs : List TensorType}
-  (expr : Expr opType args outs) (code : String) :
-    StateM (ℕ × Cached (List ℕ × String) × Cached String) (List ℕ) :=
+  (expr : Expr opType args outs) (code : String) : CodeM (List ℕ) :=
   fun ⟨n_var, codes, libs⟩ ↦
     match codes.find? (fun ⟨addr, _⟩ ↦ ptrAddrUnsafe expr == addr) with
     | none =>
@@ -91,16 +60,6 @@ unsafe def Expr.addLine {args outs : List TensorType}
     | some ⟨_, out_ids, _⟩ =>
       ⟨out_ids, n_var, codes, libs⟩
 
-unsafe def Expr.addLib {args outs : List TensorType}
-  (expr : Expr opType args outs) (code : String) :
-    StateM (ℕ × Cached (List ℕ × String) × Cached String) ℕ :=
-  fun ⟨n_var, codes, libs⟩ ↦
-    match libs.findIdx? (fun ⟨addr, _⟩ ↦ ptrAddrUnsafe expr == addr) with
-    | none =>
-      ⟨libs.length, n_var, codes, libs.concat ⟨ptrAddrUnsafe expr, code⟩⟩
-    | some i =>
-      ⟨i, n_var, codes, libs⟩
-
 def Expr.processCode (out_names : List String) (codes : Cached (List ℕ × String)) : String :=
   let body : String := "\n".intercalate <|
     codes.map fun ⟨_, assign_id, line⟩ =>
@@ -108,20 +67,30 @@ def Expr.processCode (out_names : List String) (codes : Cached (List ℕ × Stri
       s!"{assign_names} = {line}"
   s!"{body}\nreturn {",".intercalate out_names}"
 
+mutual
+
+unsafe def Expr.addLib {args outs : List TensorType}
+  (expr : Expr opType args outs) : CodeM ℕ :=
+  fun ⟨n_var, codes, libs⟩ ↦
+    match libs.findIdx? (fun ⟨addr, _⟩ ↦ ptrAddrUnsafe expr == addr) with
+    | none =>
+      let ⟨out_names, _, fn_codes, libs⟩ := expr.genCode ⟨0, [], libs⟩
+      let fn_code := processCode out_names fn_codes
+      ⟨libs.length, n_var, codes, libs.concat ⟨ptrAddrUnsafe expr, fn_code⟩⟩
+    | some i =>
+      ⟨i, n_var, codes, libs⟩
+
 unsafe def Expr.genCode {args outs : List TensorType} (expr : Expr opType args outs) :
-    StateM (ℕ × Cached (List ℕ × String) × Cached String) (List String) :=
+    CodeM (List String) :=
   match expr with
+  | nil => pure []
   | arg i => pure [s!"${i}"]
   | bind op xs => do
     let xs ← xs.genCode
     let out_id ← expr.addLine s!"{op} {",".intercalate xs}"
     return out_id.map fun n ↦ s!"%{n}"
   | apply fn xs => do
-    let ⟨n_var, codes, libs⟩ ← get
-    let ⟨out_names, _, fn_codes, libs⟩ := fn.genCode ⟨0, [], libs⟩
-    set (n_var, codes, libs)
-    let fn_code := processCode out_names fn_codes
-    let fn_id ← fn.addLib fn_code
+    let fn_id ← fn.addLib
     let xs ← xs.genCode
     let out_id ← expr.addLine s!"call @{fn_id} {",".intercalate xs}"
     return out_id.map fun n ↦ s!"%{n}"
@@ -135,7 +104,14 @@ unsafe def Expr.genCode {args outs : List TensorType} (expr : Expr opType args o
       match xs[i]? with
       | none => ""
       | some a => a
-    
+  | fori_loop n fn init => do
+    let fn ← fn.addLib
+    let init ← init.genCode
+    let out_id ← expr.addLine s!"fori_loop {n} @{fn} {",".intercalate init}"
+    return out_id.map fun n ↦ s!"%{n}"
+
+end
+
 unsafe def Expr.code {args outs : List TensorType} (expr : Expr opType args outs) : String :=
   let ⟨out_names, _, codes, libs⟩ := expr.genCode ⟨0, [], []⟩
   let body := processCode out_names codes
@@ -169,7 +145,6 @@ syntax "let_expr" ident ":" term ":=" term ";" expr_builder : expr_builder
 syntax "let" ident ":" term ":=" term ";" expr_builder : expr_builder
 syntax "let" ident ":=" term ";" expr_builder : expr_builder
 syntax "return" term : expr_builder
---syntax "fori_loop" term "," term "," term "," term : expr_builder
 
 open Lean in
 partial def parse_expr_builder (opType args : TSyntax `term) :
@@ -182,8 +157,6 @@ partial def parse_expr_builder (opType args : TSyntax `term) :
   | `(expr_builder| let $name := $val; $content) => do
     `(term| let $name := $val; $(← parse_expr_builder opType args content))
   | `(expr_builder| return $rets) => pure rets
---  | `(expr_builder| fori_loop $n, $fn , $init , $aux) =>
---    `(term| ExprGroup.fori_loop $n $fn $init $aux)
   | _ => Macro.throwUnsupported
 
 open Lean in macro_rules
@@ -201,37 +174,29 @@ inductive SimpleOp : List TensorType → TensorType → Type where
   | iota (n : ℕ) : SimpleOp [] ⟨.float, [n]⟩
   | add {σ : TensorType} : SimpleOp [σ, σ] σ
 
+instance (args : List TensorType) (out : TensorType) : ToString (SimpleOp args out) where
+  toString x :=
+    match x with
+    | .iota n => s!"iota {n}"
+    | .add => s!"add"
+
 def SimpleOp.expr_add {args : List TensorType} {σ : TensorType} (x y : Expr SimpleOp args [σ]) :
     Expr SimpleOp args [σ] :=
   let feedin : Expr SimpleOp args [σ, σ] := Expr.append x y
   Expr.bind SimpleOp.add feedin
 
+def SimpleOp.expr_iota {args : List TensorType} (n : ℕ) :
+    Expr SimpleOp args [⟨.float, [n]⟩] :=
+  Expr.bind (SimpleOp.iota n) Expr.nil
+
 def foobar (n : ℕ) :=
   define_expr using SimpleOp with
     x : ⟨.float, [n]⟩
   begin
-    return SimpleOp.expr_add x x
+    return SimpleOp.expr_add x (SimpleOp.expr_iota n)
 
 #print foobar
 
-/-
-open Lean in macro_rules
-  | `(xla with $[$argnames : $argdtypes $argshapes],*
-      returns $[$retdtypes $retshapes],*
-      begin $content) => do
-    let arglist : TSyntax `term ← `(term| [ $[⟨.$argdtypes, $argshapes⟩],* ])
-    let retlist : TSyntax `term ← `(term| [ $[⟨.$retdtypes, $retshapes⟩],* ])
-    let argId : Array (TSyntax `term) := Array.ofFn fun (i : Fin argnames.size) => quote i.val
-    let parsed : TSyntax `term ← parse_expr_builder arglist content
-    let args : List (TSyntax `ident × TSyntax `ident × TSyntax `term × TSyntax `term) :=
-      Array.toList <| argnames.zip <| argdtypes.zip <| argshapes.zip <| argId
-    let rec bind_args :
-      List (TSyntax `ident × TSyntax `ident × TSyntax `term × TSyntax `term)
-      → MacroM (TSyntax `term)
-    | [] => return parsed
-    | ⟨name, dtype, shape, id⟩ :: args =>
-      do `(let $name : Expr $arglist ⟨.$dtype, $shape⟩ := .arg $id; $(← bind_args args))
-    `(term| ( $(← bind_args args) : ExprGroup $arglist $retlist) )
--/
+#eval IO.println (foobar 10).code
 
 end Jax
